de7a01b856c84b33f9a01e71a370f030b1b9d6a9
Sorokin Alexandr <sashasorokin754@gmail.com>
2025-09-16 10:03:05 +0300

    feat: реализация структуры employee и утилиты Reporter
    - добавлена структура employee для хранения данных о сотрудниках
    - реализована утилита Reporter для формирования текстового отчёта

---
+++ Reporter/Reporter.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <vector>
+#include "employee.h"
+
+int main(int argc, char* argv[]) {
+	if (argc != 4) {
+		std::cout << "Ошибка. Неверное количество аргументов\n";
+		return 1;
+	}
+
+	std::string filename = argv[1];
+	std::string titlename = argv[2];
+
+	double salary = std::atof(argv[3]);
+	if (salary <= 0) {
+		std::cout << "Ошибка. Зарплата должна быть больше 0\n";
+	}
+
+	std::ifstream fin(filename, std::ios::binary);
+	if (!fin.is_open()) {
+		std::cout << "Ошибка. Файл " << filename << " не открылся\n";
+		return 1;
+	}
+
+	std::vector<employee> data;
+	employee empl;
+
+	while (fin.read(reinterpret_cast<char*>(&empl), sizeof(empl))) {
+		data.push_back(empl);
+	}
+	fin.close();
+
+	std::ofstream fout(titlename);
+	if (!fout.is_open()) {
+		std::cout << "Ошибка. Файл " << titlename << " не открылся\n";
+		return 1;
+	}
+
+	fout << "Отчёт по файлу\n";
+	fout << "Номер сотрудника\tимя сотрудника\tчасы\tзарплата.\n";
+	for (const auto& e : data) {
+		fout << e.num << "\t" << e.name << "\t" << e.hours << "\t" << e.hours * salary << "\n";
+	}
+
+	fout.close();
+	std::cout << "Файл " << titlename << " успешно создан\n";
+	return 0;
+}
---
+++ employee.h
@@ -0,0 +1,11 @@
+#pragma once
+#pragma pack(push, 1)
+
+struct employee
+{
+	int num; // идентификационный номер сотрудника
+	char name[10]; // имя сотрудника
+	double hours; // количество отработанных часов
+};
+
+#pragma pack(pop)
